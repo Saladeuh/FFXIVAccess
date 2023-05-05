91d3ef2b9e94854235b6c9352b24fd98d0754109
Saladeuh <emilienmonet10@gmail.com>
2023-05-05 23:47:01 +0200

    Fix project organisation

---
+++ FFXIVAccess/ScreenReader.cs
@@ -0,0 +1,49 @@
+// Not used at the moment
+using DavyKager;
+using System;
+using System.IO;
+
+namespace FFXIVAccess
+{
+    class ScreenReader
+    {
+        internal static bool Output(string text, bool interrupt = false)
+        {
+            if (text.Length == 0)
+            {
+                return false;
+            }
+
+            var success = Tolk.Output(text, interrupt);
+            return success;
+        }
+
+        internal static void Load()
+        {
+            // Append accessibility deps (e.g. Tolk, NVDA drivers, etc.) to PATH
+            var path = Environment.GetEnvironmentVariable("PATH", EnvironmentVariableTarget.Process);
+            var accessibilityAssembliesDir = Path.Combine(Directory.GetCurrentDirectory(), "Accessibility");
+            path += $";{accessibilityAssembliesDir}";
+            Environment.SetEnvironmentVariable("PATH", path, EnvironmentVariableTarget.Process);
+
+            // Load Tolk
+            Tolk.TrySAPI(true);
+            Tolk.Load();
+        }
+
+        internal static void Unload()
+        {
+            Tolk.Unload();
+        }
+
+        internal static bool IsUsingSAPI()
+        {
+            return Tolk.DetectScreenReader().Equals("SAPI");
+        }
+
+        internal static void Interrupt()
+        {
+            Tolk.Output("", true);
+        }
+    }
+}
